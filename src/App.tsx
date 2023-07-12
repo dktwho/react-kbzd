cf29a5b35e1bebe4447d8c2166255bbee6ed4d45
Alexander Korobov <dktwho@mail.ru>
2023-07-12 16:43:00 +0300

    create inside app component rating and accordion

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,37 @@ import './App.css';
 function App() {
     return (
         <div className="App">
-
+            <div>This App component</div>
+            <Rating/>
+            <Accordion />
         </div>
     );
+}
+
+function Rating() {
+    return (
+        <div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+        </div>
+
+    )
+}
+
+function Accordion() {
+    return (
+        <div>
+            <h3>Menu</h3>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    )
 }
 
 export default App;
