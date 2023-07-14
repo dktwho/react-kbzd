16bc8a1990b88c293dc8e67f240cc91d47d444cd
Alexander Korobov <dktwho@mail.ru>
2023-07-14 17:49:42 +0300

    modified Rating, add to props callback function change rating

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Accordion} from "./components/Accordion/Accordion";
-import {Rating} from "./components/Rating/Rating";
+import {Rating, RatingValueType} from "./components/Rating/Rating";
 import {OnOff} from "./components/OnOff/OnOff";
 import OnOffWithState from "./components/OnOff/OnOffWithState";
 import {UnControlledAccordion} from "./components/UnControlledAccordion/UnControlledAccordion";
 import {UnControlledRating} from "./components/UnControlledRating/UnControlledRating";
 
 function App() {
-
+    const [ratingValue, setRatingValue] = useState<RatingValueType>(0)
     return (
         <div className="App">
             <div>
@@ -16,12 +16,12 @@ function App() {
                 Article 1
                 <Accordion titleValue={'---Menu---'} collapsed={true}/>
                 <Accordion titleValue={'---Users---'} collapsed={false}/>
-                <Rating value={0}/>
-                <Rating value={1}/>
-                <Rating value={2}/>
-                <Rating value={3}/>
-                <Rating value={4}/>
-                <Rating value={5}/>
+                {/*<Rating value={0}/>*/}
+                {/*<Rating value={1}/>*/}
+                {/*<Rating value={2}/>*/}
+                {/*<Rating value={3}/>*/}
+                {/*<Rating value={4}/>*/}
+                {/*<Rating value={5}/>*/}
                 <br/>
                 <hr/>
                 <h2>Uncontrolled accordion with state inside for collapsed</h2>
@@ -34,6 +34,10 @@ function App() {
                 <OnOff status={false}/>
                 <hr/>
                 <OnOffWithState/>
+                <br/>
+                <hr/>
+                <h2>Controlled component with outside props and data</h2>
+                <Rating value={ratingValue} onClick={setRatingValue}/>
             </div>
         </div>
     );
--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 
+
+export type RatingValueType = 1 | 2 | 3 | 4 | 5 | 0
+
 type RatingPropsType = {
-    value: 1 | 2 | 3 | 4 | 5 | 0
+    value: RatingValueType
+    onClick: (value: RatingValueType) => void
 }
 
 export function Rating(props: RatingPropsType) {
     return (
         <div>
-            <Star selected={props.value > 0}/>
-            <Star selected={props.value > 1}/>
-            <Star selected={props.value > 2}/>
-            <Star selected={props.value > 3}/>
-            <Star selected={props.value > 4}/>
+            <Star selected={props.value > 0} onClick={props.onClick} value={1}/>
+            <Star selected={props.value > 1} onClick={props.onClick} value={2}/>
+            <Star selected={props.value > 2} onClick={props.onClick} value={3}/>
+            <Star selected={props.value > 3} onClick={props.onClick} value={4}/>
+            <Star selected={props.value > 4} onClick={props.onClick} value={5}/>
         </div>
     )
 }
@@ -19,11 +23,13 @@ export function Rating(props: RatingPropsType) {
 
 type StarPropsType = {
     selected: boolean
+    onClick: (value: RatingValueType) => void
+    value:  RatingValueType
 }
 
 function Star(props: StarPropsType) {
 
     return (
-        <span> {props.selected ? <b>star</b> : 'star'}</span>
+        <span onClick={() => props.onClick(props.value) }> {props.selected ? <b>star</b> : 'star'}</span>
     )
 }
