3b4f1a99ed9c856414c5a9eccd2762c2ce8e9338
Alexander Korobov <dktwho@mail.ru>
2023-07-31 15:00:21 +0300

    add storybook for component UnControlledRating

---
+++ src/components/UnControlledRating/UnControlledRating.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {UnControlledRating} from "./UnControlledRating";
+
+export default  {
+    title: 'UnControlledRating',
+    component: UnControlledRating,
+}
+
+export const ModeChanging = () => {
+
+     return <UnControlledRating />
+
+}
