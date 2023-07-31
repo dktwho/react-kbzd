26012aa757f313ef313b4fc70cf04058f20e1415
Alexander Korobov <dktwho@mail.ru>
2023-07-31 14:41:12 +0300

    fixed storybook for component Rating

--- src/components/Rating/Rating.stories.tsx
+++ src/components/Rating/Rating.stories.tsx
@@ -9,14 +9,14 @@ export default  {
 
 
 export const EmptyStars = () => <Rating value={0} setRatingValue={x=>x} />
-export const Rating1 = () => <Rating value={1} />
-export const Rating2 = () => <Rating value={2} />
-export const Rating3 = () => <Rating value={3} />
-export const Rating4 = () => <Rating value={4}  />
-export const Rating5 = () => <Rating value={5} />
+export const Rating1 = () => <Rating value={1} setRatingValue={() => {}} />
+export const Rating2 = () => <Rating value={2} setRatingValue={() => {}}/>
+export const Rating3 = () => <Rating value={3} setRatingValue={() => {}}/>
+export const Rating4 = () => <Rating value={4}  setRatingValue={() => {}}/>
+export const Rating5 = () => <Rating value={5} setRatingValue={() => {}}/>
 
 export const ChangeRating = () => {
     const [rating, setRatingValue] = useState<RatingValueType>(3)
-    return  <Rating value={rating} />
+    return  <Rating value={rating} setRatingValue={setRatingValue} />
 
 }
