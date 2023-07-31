96966da831d8ace8f6e4320788b98bc701a1c51a
Alexander Korobov <dktwho@mail.ru>
2023-07-31 14:59:19 +0300

    add storybook for component OnOffWithState

--- src/components/UnControlledOnOff/OnOffWithState.tsx
+++ src/components/UnControlledOnOff/OnOffWithState.tsx
@@ -2,17 +2,22 @@ import React, {useState} from 'react';
 import styled from './OnOffWithState.module.css'
 
 
-const OnOffWithState = () => {
+export const OnOffWithState = () => {
     const [indicator, setIndicator] = useState(false)
 
     return (
         <div className={styled.container}>
-            <button className={indicator ? `${styled.activeBtn}` : `${styled.templateBtn}`} onClick={() => setIndicator(true)}>On</button>
-            <button className={!indicator ? styled.inActiveBtn : styled.templateBtn } onClick={() => {setIndicator(false)}}>Off</button>
-            <div onClick={() => setIndicator(!indicator)} className={ indicator ?  styled.indicatorActive : styled.indicatorInActive }></div>
+            <button className={indicator ? `${styled.activeBtn}` : `${styled.templateBtn}`}
+                    onClick={() => setIndicator(true)}>On
+            </button>
+            <button className={!indicator ? styled.inActiveBtn : styled.templateBtn} onClick={() => {
+                setIndicator(false)
+            }}>Off
+            </button>
+            <div onClick={() => setIndicator(!indicator)}
+                 className={indicator ? styled.indicatorActive : styled.indicatorInActive}></div>
 
         </div>
     );
 };
 
-export default OnOffWithState;
---
+++ src/components/UnControlledOnOff/UnControlledOnOffWithState.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {OnOffWithState} from "./OnOffWithState";
+
+export default  {
+    title: 'OnOffWithState',
+    component: OnOffWithState,
+}
+
+export const ModeChanging = () => {
+
+     return <OnOffWithState />
+
+}
