b7dabdbebb22de6f6fba027eb1beb824ba0213c3
Alexander Korobov <dktwho@mail.ru>
2023-07-31 14:36:01 +0300

    modified2 StoryBook, test for component Accordion, check ol method action

--- src/components/Accordion/Accordion.stories.tsx
+++ src/components/Accordion/Accordion.stories.tsx
@@ -1,7 +1,5 @@
 import React, {useState} from 'react';
 import {Accordion} from "./Accordion";
-import { action } from '@storybook/addon-actions';
-
 
 export default  {
     title: 'Accordion',
@@ -13,6 +11,6 @@ export const UnCollapsedMode = () => <Accordion titleValue={'User'} accordion={f
 
 export const ModeChanging = () => {
     const [value, setValue] = useState<boolean>(false)
-    return <Accordion accordion={value} setAccordion={() => setValue(!value)} titleValue={'Users'} />
+    return <Accordion accordion={value} setAccordion={setValue} titleValue={'Users'} />
 
 }
