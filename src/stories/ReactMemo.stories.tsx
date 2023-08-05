0d62c50c8f3d0a10debd9ae1744abeb636688492
Alexander Korobov <dktwho@mail.ru>
2023-08-05 14:47:54 +0300

    add button with logic added new user in users , and rerendered.

--- src/stories/ReactMemo.stories.tsx
+++ src/stories/ReactMemo.stories.tsx
@@ -11,16 +11,16 @@ const NewMessagesCounter = (props: {count: number}) => {
     return <div>{props.count}</div>
 }
 const UsersSecret = (props: UsersType) => {
-    console.log('render users')
     return <div>{props.users.map((u,i) => <div key={i}>{u}</div>)}</div>
 }
 
 const Users = React.memo(UsersSecret)
 export const Example1 = () => {
     const [counter, setCounter] = useState(0)
-    const [users, setusers] = useState(['Bob', 'Bill', 'Sam'])
+    const [users, setUsers] = useState(['Bob', 'Bill', 'Sam'])
     return <>
-        <button onClick={() => setCounter(counter + 1)}>+</button>
+        <button onClick={() => setCounter(counter + 1) }>+</button>
+        <button onClick={() => setUsers([...users, 'NEW USER']) }>add user</button>
         <NewMessagesCounter count={counter}/>
         <Users users={users}/>
     </>
