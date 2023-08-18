3e9e64248c6dfc2ace511d61bea5cbe22658904d
Alexander Korobov <dktwho@mail.ru>
2023-08-18 12:53:33 +0300

    add stories for UseMemo, example two input with factorial

---
+++ src/stories/UseMemeo.stories.tsx
@@ -0,0 +1,31 @@
+import React, {useState} from "react";
+
+export default {
+    title: 'UseMemo'
+}
+
+export const Example1 = () => {
+    const [a, setA] = useState(5)
+    const [b, setB] = useState(5)
+    
+    let resultA = 1;
+    let resultB = 1;
+
+    for (let i = 1; i <= a  ; i++) {
+        resultA = resultA * i
+    }
+
+    for (let i = 1; i <= b ; i++) {
+        resultB = resultB * i
+    }
+    
+    return (
+        <>
+            <input value={a} onChange={(e) => setA(Number(e.currentTarget.value))}/>
+            <input value={b} onChange={(e) => setB(Number(e.currentTarget.value))}/>
+            <hr/>
+            <div>result for a: {resultA}</div>
+            <div>result for b: {resultB}</div>
+        </>
+    )
+}
