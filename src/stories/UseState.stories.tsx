9faf7b2362b0225b059fd6a4aaf7bda2ffad4493
Alexander Korobov <dktwho@mail.ru>
2023-08-31 16:45:41 +0300

    add to stories file UseState stories, practice with hook useState, with function changer

--- src/stories/UseState.stories.tsx
+++ src/stories/UseState.stories.tsx
@@ -8,12 +8,31 @@ function generateData () {
     return 235648965
 }
 
+function generateData2 () {
+    console.log('generate difficult value')
+    return 1111333322222
+}
+
+
 export const Example1 = () => {
     // передаем в стейт функции которую useSTate вызывает сам, и запоминает значение, как useMemo но при условии что функция синхронная
     const [counter, setCounter] = useState(generateData)
     console.log('Example1')
     return <>
         <button onClick={() => setCounter(counter + 1) }>+</button>
+        <h1>{counter}</h1>
+    </>
+}
+
+export const Example2 = () => {
+    // пример с функцией изменителем
+    const [counter, setCounter] = useState(generateData2)
+    console.log('Example2')
+
+    const changer = (state: number) => state + 1
+    return <>
+        {/*<button onClick={() => setCounter(changer) }>+</button>*/}
+        <button onClick={() => setCounter(state => state + 1) }>+</button>
         <h1>{counter}</h1>
     </>
 }
