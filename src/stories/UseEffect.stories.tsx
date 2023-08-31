bd4bafe04f3ae4867ed1469e64cdf745b35744f3
Alexander Korobov <dktwho@mail.ru>
2023-08-31 17:16:32 +0300

    add to stories file UseEffect stories, practice with hook useEffect, with  dependencies, with setInterval and setTimeout (async)

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -12,12 +12,12 @@ export const UseEffect = () => {
     useEffect(() => {
         console.log('useEffect when dependencies not defined , EVERY RENDER ')
         document.title = counter.toString()
-    } )
+    })
 
     useEffect(() => {
         console.log('useEffect when dependencies array, ONLY FIRST RENDER WHEN COMPONENT IS MOUNTING')
         document.title = counter.toString()
-    }, [] )
+    }, [])
 
     useEffect(() => {
         console.log('useEffect when dependencies is some value, FIRST RENDER when dependencies is CHANGE')
@@ -26,6 +26,33 @@ export const UseEffect = () => {
     return <>
         <button onClick={() => setFake(fake + 1)}>+</button>
         <button onClick={() => setCounter(counter + 1)}>+</button>
-        Hello   <h1>{counter}</h1> <h1>{fake}</h1>
+        Hello <h1>{counter}</h1> <h1>{fake}</h1>
+    </>
+}
+export const SetTimeoutExample = () => {
+    const [counter, setCounter] = useState(1)
+    const [fake, setFake] = useState(1)
+    console.log('Example12')
+
+    useEffect(() => {
+        setTimeout(() => {
+            console.log('setTimeout called')
+            document.title = counter.toString()
+        }, 1000)
+    }, [counter])
+
+    // useEffect(() => {
+    //     setInterval(() => {
+    //         setCounter((state) => state + 1)
+    //     }, 1000)
+    // }, [])
+
+    return <>
+        <button onClick={() => setFake(fake + 1)}>fake click</button>
+        <button onClick={() => setCounter(counter + 1)}>counter click</button>
+        Hello <h1>{counter}</h1> <h1>{fake}</h1>
+        <br/>
+        SetInterval
+       <div> {fake} - {counter}</div>
     </>
 }
