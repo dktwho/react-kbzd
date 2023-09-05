9980215d6134f5bb0b9883b11b1b2dcedcbbe851
Alexander Korobov <dktwho@mail.ru>
2023-09-05 15:49:30 +0300

    add clearInterval for second arguments inside callback in useEffect

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -3,14 +3,19 @@ import React, {useEffect, useState} from "react";
 
 type PropsType = {}
 
-const getTwoDigitsString = (num: number) => num < 10 ? `0 + ${num}` : num
+const getTwoDigitsString = (num: number) => num < 10 ? `0${num}` : num
 export const Clock: React.FC<PropsType> = (props) => {
     const [date, setDate] = useState(new Date)
 
     useEffect(() => {
-        setInterval(() => {
+        const intevalId = setInterval(() => {
+            console.log('tick')
             setDate(new Date())
         }, 1000)
+
+        return () => {
+            clearInterval(intevalId)
+        }
     }, [])
 
     return (
